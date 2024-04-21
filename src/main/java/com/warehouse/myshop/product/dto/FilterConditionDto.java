8c41ba9ebb4a591fc751d0c536a77297944f0857
Roman <madara-09@bk.ru>
2024-04-21 19:15:32 +0300

    feature: частична реализована стратегия для Specification

--- src/main/java/com/warehouse/myshop/product/dto/FilterConditionDto.java
+++ src/main/java/com/warehouse/myshop/product/dto/FilterConditionDto.java
@@ -3,11 +3,17 @@ package com.warehouse.myshop.product.dto;
 import com.fasterxml.jackson.annotation.*;
 import com.warehouse.myshop.product.enums.Operation;
 import com.warehouse.myshop.product.dto.FilterConditionDto.*;
+import com.warehouse.myshop.product.model.Product;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.jpa.domain.Specification;
 
+import javax.persistence.criteria.CriteriaBuilder;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.EXISTING_PROPERTY, property = "field", visible = true)
 @JsonSubTypes({
@@ -19,16 +25,31 @@ import java.time.LocalDateTime;
 @Setter
 @Getter
 public abstract class FilterConditionDto<T> {
-    private String field;
-    private T value;
-    private Operation operation;
+    protected String field;
+    protected T value;
+    protected Operation operation;
+
+    abstract Specification<Product> getSpecification();
 
 
     public void setOperation(String operation) {
         this.operation = Operation.fromString(operation);
     }
 
-    public static class NumericFilterConditionDto extends  FilterConditionDto<BigDecimal>{}
+    public static class NumericFilterConditionDto extends  FilterConditionDto<BigDecimal>{
+        @Override
+        Specification<Product> getSpecification() {
+            switch (this.operation) {
+                case EQUALS:
+                case LIKE:
+                    return (root, query, cb) -> cb.equal(root.get(this.field), this.value);
+                case GREATER_THAN_OR_EQUALS:
+                    return (root, query, cb) -> cb.greaterThanOrEqualTo(root.get(this.field), this.value);
+                case LESS_THAN_OR_EQUALS:
+                    return (root, query, cb) -> cb.lessThanOrEqualTo(root.get(this.field), this.value);
+            }
+        }
+    }
 
     public static class DateFilterConditionDto extends FilterConditionDto<LocalDateTime> {
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm:ss dd.MM.yyyy")
